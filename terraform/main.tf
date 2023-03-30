e33ab0ccc4c256c18d7933bcb08965f4e8f1e593
Sören Christ <s.christ@mailbox.org>
2023-03-30 06:46:55 +0000

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+    organization = "schrist"
+
+    workspaces {
+      tags = ["resume", "azure"]
+    }
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
