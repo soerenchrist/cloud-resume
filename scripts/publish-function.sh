e5186c91ecbe2360898ecc4602112f20e37452a9
Sören Christ <s.christ@mailbox.org>
2023-03-30 07:39:21 +0000

    Add publish function

--- scripts/publish-function.sh
+++ scripts/publish-function.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+ENV=$1
+
+if [ -z $ENV ]; then ENV="dev"; fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-FUNCTION_APP="resume-backend-dev-function-app"
+FUNCTION_APP="resume-backend-$prod-function-app"
 PROJECT_DIR="$SCRIPT_DIR/../backend"
 
 echo "Publish Function"
